aa52fdfe8880834374cf9e398ff766a8e6fbd18d
Victorqqeeee <113634377+Victorqqeeee@users.noreply.github.com>
2023-03-20 16:51:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+video =""
+
+function preload(){
+    video = createVideo('video(1).mp4');
+    video.hide
+}
+
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center();
+}
+
+function draw (){
+    image(video, 0, 480, 380);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status Detectando Objetos";
+}
+
+function modelLoaded(){
+    console.log("Modelo Carregado!");
+    status = true;
+    video.loop;
+    video.speed(1);
+    video.volume(0);
+}
